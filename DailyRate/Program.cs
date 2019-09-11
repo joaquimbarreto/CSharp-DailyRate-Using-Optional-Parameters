40024b5c9eb6686b353a0ba79c9943673926194c
Joaquim Barreto <joaquim@barreto.co>
2019-09-11 10:08:36 +0200

    Done

--- DailyRate/Program.cs
+++ DailyRate/Program.cs
@@ -15,7 +15,19 @@ namespace DailyRate
 
         void run()
         {
-
+            double fee = calculateFee(500.0, 3);
+            Console.WriteLine($"Fee is  {fee}");
+        }
+        private double calculateFee(double dailyRate = 500.0, int noOfDays = 1)
+        {
+            Console.WriteLine("calculateFee using two optimal parameters");
+            return dailyRate * noOfDays;
+        }
+        private double calculateFee(double dailyRate = 500.0)
+        {
+            Console.WriteLine("calculateFee using two optimal parameters");
+            int defaultNoOfDays = 1;
+            return dailyRate * defaultNoOfDays;
         }
     }
 }
